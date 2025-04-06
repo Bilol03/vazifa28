85bb57922393f1e92f98b769f0f0111d08649b1a
Bilol <madiyarovbilol78@gmail.com>
2025-04-06 20:27:05 +0500

    LOGIN has been finished

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -28,7 +28,52 @@ let REGISTER = errorHandler(async (req, res, next) => {
 	responce(res, 201, { message: 'Successfully registered!', user })
 })
 
+
+let LOGIN = errorHandler(async (req, res, next) => {
+	let { username, password } = req.body
+	if (!username || !password)
+		throw new Error('Username or Passwor not entered')
+	let user = await User.findOne({ username })
+		.select('password username email firstName role')
+		.exec()
+	if (!user) throw new Error('User not registered ')
+
+	console.log(req.body)
+
+	let checking = await bcrypt.compare(password, user.password)
+	if (!checking) throw new Error('Wrong password!')
+	user.password = password
+
+	let token = jwt.sign(
+		{ id: user.id, role: user.role },
+		process.env.SECRET_KEY,
+		{ expiresIn: process.env.JWT_EXP_TIME },
+	)
+
+	let refreshToken = jwt.sign(
+		{ id: user.id, role: user.role },
+		process.env.JWT_REFRESH_TOKEN_SECRET_KEY,
+		{ expiresIn: eval(process.env.JWT_REFRESH_EXP_TIME) },
+	)
+	user.refreshToken = refreshToken
+	await user.save()
+
+	let options = {
+		maxAge: eval(process.env.JWT_REFRESH_EXP_TIME),
+		httpOnly: false,
+	}
+
+	res.cookie('jwt', refreshToken, options)
+
+	let userObj = user.toObject()
+
+	delete userObj.password
+	delete userObj.refreshToken
+
+	responce(res, 200, { userObj, token })
+})
+
 export default {
     REGISTER,
-
+    LOGIN
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,6 @@ import authController from '../controllers/auth.controller.js'
 let route = Router()
 
 route.post('/register', authController.REGISTER)
+route.post('/login', authController.LOGIN)
 
 export default route
